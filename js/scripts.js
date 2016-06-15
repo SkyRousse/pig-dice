8d40d24da5881adafd6d8f7ec9cac298e757e160
Marty Fitchen and Sky Rousse <sky@>
2016-06-15 16:47:36 -0700

    add dice roll functionality and poutput

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,71 @@
 //business logic
-var active = true;
+var active;
 var roundScore = 0;
 var gameScore = 0;
 
-function Player(name, turn, total, active) {
+function Player(name, active) {
   this.playerName = name;
-  this.turnScore = turn;
-  this.totalScore = total;
   this.activePlayer = active;
-}
+  this.turnScore = 0;
+  this.totalScore = 0;
+};
 
 var diceRoll = function() {
-    var rollNumber = Math.floor((Math.random() * 6) + 1);
-    return rollNumber
-}
+  var rollNumber = Math.floor((Math.random() * 6) + 1);
+  return rollNumber;
+};
 
-// Player.prototype.rollScore = function() {
-//   if/else or loop or equation
-// }
+var getCurrentPlayer = function() {
+  if(this.activePlayer === true) {
+    currentplayer = this.activePlayer
+  }
+};
 
+//idea: add players to an array to loop through
 Player.prototype.turnScore = function() {
+//while loop to check active is true execute code for turnScore
   if(diceRoll !== 1) {
     roundScore += diceRoll;
   } else {
     this.activePlayer === false;
+    roundScore === 0;
   }
   this.turnScore = roundScore;
-}
+};
 
 Player.prototype.totalScore = function() {
   gameScore += roundScore;
   this.totalScore === gameScore;
-}
+};
 
 //user interface logic
 $(document).ready(function() {
+  var player1;
+  var player2;
   $("form#player-setup-1").submit(function(event) {
     event.preventDefault();
-    var player1 = $("input.player-1-name").val();
-    // pigPlayer = new Player(player1)
-    $("h3.output-player-1-name").text(player1);
+    var player1Name = $("input.player-1-name").val();
+    player1 = new Player(player1Name, true);
+    console.log(player1.playerName);
+    $("h3.output-player-1-name").text(player1.playerName);
   });
   $("form#player-setup-2").submit(function(event) {
     event.preventDefault();
-    var player2 = $("input.player-2-name").val();
-    // pigPlayer = new Player(player2)
-    $("h3.output-player-2-name").text(player2);
+    var player2name = $("input.player-2-name").val();
+    player2 = new Player(player2name, false);
+    $("h3.output-player-2-name").text(player2.playerName);
   });
+
+  $("button.btn-roll-dice").click(function() {
+    var rollNumber = diceRoll();
+    console.log(rollNumber);
+    var currentplayer = getCurrentPlayer();
+    // Player.turnScore(diceRoll);
+    $("h3.output-dice-roll").text(rollNumber);
+    $("h3.output-turn-total").text(rollNumber);
+  });
+  $("button.btn-feed-the-pig").click(function() {
+    Player.totalScore(Player.turnScore());
+  });
+
 });
